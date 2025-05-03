97315b9e5f53a1fb81833a9ab39da844c84cd092
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2025-05-03 21:42:42 +0200

    Write data writing

--- punctuation_tokenizer.py
+++ punctuation_tokenizer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
+import array
+import io
 import string
+import struct
 import sys
 from typing import List, Iterator, Tuple
+
+UINT64_MAX = 0xFFFF_FFFF_FFFF_FFFF
+UINT32_MAX = 0xFFFF_FFFF
+UINT16_MAX = 0xFFFF
+UINT8_MAX = 0xFF
 
 UPPER =       0b0000010
 LOWER =       0b0000001
@@ -59,6 +67,22 @@ def tokenize_name(name: str) -> Iterator[Tuple[int, str]]:
         yield classify_token(token), token
 
 
+def numbers_to_array(numbers: List[int]) -> array.ArrayType:
+    assert min(numbers) >= 0
+    number_max = max(numbers)
+    if number_max > UINT64_MAX:
+        raise NotImplementedError("Numbers to big")
+    if number_max > UINT32_MAX:
+        array_type = "Q"
+    elif number_max > UINT16_MAX:
+        array_type = "I"
+    elif number_max > UINT8_MAX:
+        array_type = "H"
+    else:
+        array_type = "B"
+    return array.array(array_type, numbers)
+
+
 class TokenStore:
     tp: int
     tokens: List[str]
@@ -66,6 +90,78 @@ class TokenStore:
     def __init__(self, tp: int, tokens: List[str]):
         self.tp = tp
         self.tokens = tokens
+
+    def to_data(self) -> bytes:
+        number_of_tokens = len(self.tokens)
+        if self.tp & PUNCTUATION:
+            token_set = set(self.tokens)
+            if len(token_set) != 1:
+                raise NotImplementedError(
+                    f"PUNCTUATION separators should be the same: {token_set}")
+            separator = token_set.pop()
+            if len(separator) != 1:
+                raise RuntimeError(
+                    f"Programmer messed up! Separator length is not 1: {separator}")
+            data = separator.encode("latin-1")
+        elif self.tp & STRING == STRING:
+            all_string = "\x00".join(self.tokens)
+            number_of_tokens = len(all_string)
+            data = (all_string.encode("latin_1"))
+        elif self.tp & LOWER or self.tp & UPPER or self.tp == DECIMAL or self.tp == ZERO_PREFIX:
+            if self.tp & LOWER or self.tp & UPPER:
+                numbers = [int(x, 16) for x in self.tokens]
+            else:
+                numbers = [int(x, 10) for x in self.tokens]
+            arr = numbers_to_array(numbers)
+            data = arr.typecode.encode("latin-1") + arr.tobytes()
+            if self.tp & ZERO_PREFIX:
+                length_set = set(len(x) for x in self.tokens)
+                if len(length_set) != 0:
+                    raise ValueError("Zero prefixed numbers should all have the same formatted length.")
+                formatted_length = length_set.pop()
+                data = struct.pack("B", formatted_length) + data
+        else:
+            raise NotImplementedError(f"Unkown token type: {self.tp}")
+        header = struct.pack("<BI", self.tp, number_of_tokens)
+        return header + data
+
+    @classmethod
+    def from_stream(cls, stream: io.BufferedIOBase):
+        tp, number_stored = struct.unpack("<BI", stream.read(5))
+        if tp & PUNCTUATION:
+            character = stream.read(1).decode("latin-1")
+            return cls(tp, [character for _ in range(number_stored)])
+        if tp & STRING == STRING:
+            all_strings = stream.read(number_stored).decode("latin-1")
+            return cls(tp, all_strings.split("\x00"))
+        # The rest is numbers
+        if tp & UPPER:
+            format_code = "X"
+        elif tp & LOWER:
+            format_code = "x"
+        else:
+            format_code = "d"
+        if tp & ZERO_PREFIX:
+            formatted_length, = struct.unpack("B", stream.read(1))
+            format_code = f"0{formatted_length}" + format_code
+        array_type = stream.read(1).decode("latin-1")
+        if array_type == "B":
+            item_size = 1
+        elif array_type == "H":
+            item_size = 2
+        elif array_type == "I":
+            item_size = 4
+        elif array_type == "Q":
+            item_size = 8
+        else:
+            raise ValueError(f"Unrecognized array_type: {array_type}")
+        bytes_stored = number_stored * item_size
+        array_bytes = stream.read(bytes_stored)
+        number_array = array.array(array_type)
+        number_array.frombytes(array_bytes)
+
+        tokens = [f"%{format_code}" % number for number in number_array]
+        return cls(tp, tokens)
 
     @classmethod
     def from_token_stream(cls, token_stream: List[tuple[int, str]]):
@@ -75,7 +171,6 @@ class TokenStore:
             combined |= tp
             tokens.append(token)
         return cls(combined, tokens)
-
 
 
 def main():
@@ -93,11 +188,10 @@ def main():
     if length_mismatch:
         raise ValueError("Unequal token lengths. Codec unsuitable.")
     token_streams = [list(row) for row in zip(*token_strings)]
-    for token_stream in token_streams:
-        print(set(TOK_TYPE_TO_STRING[tp] for tp, token in token_stream))
     token_stores = [TokenStore.from_token_stream(token_stream)
                     for token_stream in token_streams]
-    print([TOK_TYPE_TO_STRING[ts.tp] for ts in token_stores])
+    all_data = b"".join(ts.to_data() for ts in token_stores)
+    sys.stdout.buffer.write(all_data)
 
 
 if __name__ == "__main__":
