e649202ca5c374ccd2af0bd1c63bc7b39100d1ab
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2025-05-03 20:24:23 +0200

    Use bit operations for token types

--- punctuation_tokenizer.py
+++ punctuation_tokenizer.py
@@ -1,95 +1,69 @@
 #!/usr/bin/env python3
 import string
 import sys
-from typing import List, Iterator
+from typing import List, Iterator, Tuple
 
-class Token:
-    tok: str
+UPPER =       0b0000010
+LOWER =       0b0000001
+DECIMAL =     0b0000000
+ZERO_PREFIX = 0b0000100
+STRING = UPPER | LOWER
+PUNCTUATION = 0b0001000
+TOK_TYPE_TO_STRING = [
+    "DECIMAL",
+    "LOWERHEXADECIMAL",
+    "UPPER_HEXADECIMAL",
+    "STRING",
+    "DECIMAL0",
+    "LOWERHEXADECIMAL0",
+    "UPPERGEXADECIMAL0",
+    "STRING",
+    "PUNCTUATION",
+]
+TOK_TYPE_TO_STRING[UPPER] = "UPPERHEXADECIMAL"
+TOK_TYPE_TO_STRING[LOWER] = "LOWERHEXADECIMAL"
+TOK_TYPE_TO_STRING[DECIMAL] = "DECIMAL"
 
-    def __init__(self, tok: str):
-        self.tok = tok
 
-class StringToken(Token):
-    pass
+def classify_token(tok: str) -> int:
+    tp = 0
+    if tok[0] == '0':
+        tp |= ZERO_PREFIX
+    for char in tok:
+        if char in string.digits:
+            tp |= DECIMAL
+        elif char in string.hexdigits:
+            if char.isupper():
+                tp |= UPPER
+            else:
+                tp |= LOWER
+        else:
+            return STRING
+    return tp
 
-class DecimalToken(Token):
-    pass
 
-class ZeroDecimalToken(Token):
-    pass
-
-class UpperHexaDecimalToken(Token):
-    pass
-
-class LowerHexaDecimalToken(Token):
-    pass
-
-class ZeroUpperHexaDecimalToken(Token):
-    pass
-
-class ZeroLowerHexaDecimalToken(Token):
-    pass
-
-class PunctutationToken(Token):
-    pass
-
-def classify_token(tok: str) -> Token:
-    if tok.isnumeric():
-        if tok.startswith("0"):
-            return ZeroDecimalToken(tok)
-        return DecimalToken(tok)
-    if set(tok).issubset(set(string.hexdigits)):
-        if tok.isupper():
-            if tok.startswith("0"):
-                return ZeroUpperHexaDecimalToken(tok)
-            return UpperHexaDecimalToken(tok)
-        elif tok.islower():
-            if tok.startswith("0"):
-                return ZeroLowerHexaDecimalToken(tok)
-            return LowerHexaDecimalToken(tok)
-    return StringToken(tok)
-
-def tokenize_name(name: str) -> Iterator[Token]:
+def tokenize_name(name: str) -> Iterator[Tuple[int, str]]:
     start = 0
     index = 0
     end = len(name)
     while index < end:
         if name[index] in string.punctuation:
             if index > start:
-                yield classify_token(name[start:index])
-            yield PunctutationToken(name[index])
+                token = name[start:index]
+                yield classify_token(token), token
+            yield PUNCTUATION, name[index]
             start = index + 1
         index += 1
     if index > start:
-        yield classify_token(name[start:index])
+        token = name[start:index]
+        yield classify_token(token), token
 
 
-def homogenize_token_stream(token_stream: List[Token]):
-    token_set = set(type(x) for x in token_stream)
-    if len(token_set) == 1:
-        return token_stream
-    max_class = StringToken
-    if token_set.issubset({
-        LowerHexaDecimalToken,
-        ZeroLowerHexaDecimalToken,
-        DecimalToken,
-        ZeroDecimalToken,
-     }):
-        max_class = ZeroLowerHexaDecimalToken
-    if token_set.issubset({
-        UpperHexaDecimalToken,
-        ZeroUpperHexaDecimalToken,
-        DecimalToken,
-        ZeroDecimalToken,
-     }):
-        max_class = ZeroUpperHexaDecimalToken
-    if token_set == {LowerHexaDecimalToken, DecimalToken}:
-        max_class = LowerHexaDecimalToken
-    if token_set == {UpperHexaDecimalToken, DecimalToken}:
-        max_class = UpperHexaDecimalToken
-    if token_set == {ZeroDecimalToken, DecimalToken}:
-        max_class = ZeroDecimalToken
-    return [max_class(token.tok) for token in token_stream]
+def homogenize_token_stream(token_stream: List[Tuple[int, str]]):
+    combined = 0
+    for tp, token in token_stream:
+        combined |= tp
+    return [(combined, token) for tp, token in token_stream]
 
 
 def main():
@@ -107,9 +81,11 @@ def main():
     if length_mismatch:
         raise ValueError("Unequal token lengths. Codec unsuitable.")
     token_streams = [list(row) for row in zip(*token_strings)]
-    token_streams = [homogenize_token_stream(x) for x in token_streams]
     for token_stream in token_streams:
-        print(set(type(x) for x in token_stream))
+        print(set(TOK_TYPE_TO_STRING[tp] for tp, token in token_stream))
+    token_streams = [homogenize_token_stream(x) for x in token_streams]
+    print([TOK_TYPE_TO_STRING[token_stream[0][0]] for token_stream in token_streams])
+
 
 if __name__ == "__main__":
     main()
